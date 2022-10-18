a03261ab6aac689394110848ac04d9c286afbf32
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-10-18 18:23:34 +0200

    heroku hecho?

---
+++ models/Coctel.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const coctelSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true
+    },
+    alcohol:{
+      type: String
+      // enum:["Whisky", "Vodka", "Tequila", "Ron", "Ginebra", "Pisco", "Orujo","Brandy", "Coñac", "Cointreau", "Licor de grosella", "Champagne", "Cava","Vermut","Vino", "Licor de fruta"]
+    },
+    ingredientes: String,
+    procedimiento: String,
+    descripcion: String,
+    origen: String,
+    puntuacion: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5]
+    },
+    imageUrl: String,
+    creador: {type: Schema.Types.ObjectId, ref: "User"}
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Coctel = model("Coctel", coctelSchema);
+
+module.exports = Coctel;
+
---
+++ routes/index.routes.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const router = express.Router();
+
+const capitalize = require("../utils/capitalize");
+
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+const User = require("../models/User.model");
+const Coctel = require("../models/Coctel.model");
+const fileUploader = require('../config/cloudinary.config');
+
+const path = require("path");
+
+
+
+/* GET home page */
+router.get("/", isLoggedOut, (req, res, next) => {
+  res.render("index");
+});
+
+
+router.get("/principal", isLoggedIn, (req, res, next) => {
+
+  Coctel.find()
+  .then(resp => {
+    const data = { 
+      coctel: resp,
+      usuari: req.session.currentUser
+    }
+    console.log("req.session.currentUser - principal: ", req.session.currentUser)
+    res.render("principal", data);
+  })
+  .catch(err => {
+    console.log("error render principal: ", err)
+  })
+});
+
+
+router.get("/profile",isLoggedIn, (req, res, next) => {
+  const user = req.session.currentUser.username
+  const userId = req.session.currentUser._id
+  console.log("aaaa :",user)
+  Coctel.find({creador: userId})
+  .then(results =>{
+    const dady = {
+      coctelUser: results,
+      user:user
+    }
+    console.log("proba based: ",results)
+    res.render("profile",dady);
+  })
+
+});
+
+
+router.get("/crear", isLoggedIn, (req, res, next) => {
+  res.render("crear")
+})
+
+router.post("/crear", isLoggedIn, fileUploader.single('coctel-cover-image'), (req, res) => {
+  const { name, alcohol, ingredientes, procedimiento, descripcion, origen } = req.body;
+  console.log("req.file", req.file);
+  const paraulaAlcohol = capitalize(alcohol)
+  
+  Coctel.create({ name, alcohol: paraulaAlcohol, ingredientes, procedimiento, descripcion,origen, imageUrl: req.file.path , creador: req.session.currentUser._id})
+    .then(newlyCreatedCoctelFromDB => {
+        console.log("newlyCreatedCoctelFromDB: ", newlyCreatedCoctelFromDB);
+    
+    res.redirect("/principal")
+  })
+});
+
+
+
+router.get("/informacion/:coctelId", isLoggedIn, (req, res, next) => {
+  const coctelId = req.params.coctelId
+  Coctel.findById(coctelId)
+    .populate("creador")
+    .then(result => {
+      const data = {
+      coctel: result
+    }
+    res.render("informacion", data)
+  })
+  })
+
+
+router.get("/logout", isLoggedIn, (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/")
+})
+
+router.get("/search", isLoggedIn, (req, res, next) => {
+  console.log("req.query search: ", req.query.alcohol)
+  const paraula = capitalize(req.query.alcohol)
+  const filtre = { alcohol: {$regex : `.*${paraula}.*`}};
+  console.log("filtre regex: ", filtre)
+  Coctel.find(filtre)
+  .then(resp => {
+    const data = { 
+      coctel: resp,
+      usuari: req.session.currentUser
+    }
+    res.render("principal", data);
+  })
+})
+
+module.exports = router;
